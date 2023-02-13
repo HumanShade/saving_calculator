cc2fe935839fb06daf08c6a03a428cf8a896620b
Norman Janurberg <njanurberg@gmail.com>
2023-02-13 17:49:15 +0100

    Version 1

---
+++ calculation_functions.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+def total_interest_cost(loan_amount, payments_number, nominal_interest=0, added_mortgage=0, fixed_mortage_cost=0):
+        remaining_loan = loan_amount
+        total_interest = 0
+        months_taken = 0
+        months_remaining = payments_number
+        while remaining_loan > 0:
+            interest_payment = remaining_loan / 12 * nominal_interest
+            if fixed_mortage_cost == 0:
+                mortgage_payment = remaining_loan / months_remaining + added_mortgage
+            else:
+                if fixed_mortage_cost < remaining_loan / months_remaining:
+                    mortgage_payment = remaining_loan / months_remaining
+                else:
+                    mortgage_payment = fixed_mortage_cost
+
+            total_interest += interest_payment
+            remaining_loan -= mortgage_payment
+            months_taken += 1
+            months_remaining = payments_number - months_taken
+
+        return int(np.round(total_interest)), months_taken
+
+def stock_growth(monthly_deposit, interest_rate, months_saving):
+        total_saved = 0
+        monthly_interest_rate = np.power(1+interest_rate, 1/12)
+
+        for _ in range(months_saving):
+            total_saved *= monthly_interest_rate
+            total_saved += monthly_deposit
+        
+        return  int(np.round(total_saved))
+
+# TODO: Add belåningsgrad, 2 % amortering when above 70 % belåningsgrad, 1 % when above 50 %, 0 % otherwise
+# TODO: Add monthly cost as return value: list of (interest payment + mortgage) per month
+# TODO: Add ränteavdrag
+# TODO: Break down the function into smaller functions for readability
+# TODO: Add increased house valuation (Separate function)
+def saving_plan(loan_amount, payments_number, nominal_interest=0, saving_amount=0, mortgage_payment=0, interest_rate=0, early_stopping=False):
+        # Housing
+        remaining_loan = loan_amount
+        loan_interest_paid = 0
+        months_taken = 0
+
+        # Stock market
+        total_invested = 0
+        stock_valuation = 0
+        monthly_interest_rate = np.power(1 + interest_rate, 1/12)
+        stock_interest_earned = 0
+        monthly_saving = 0
+
+        if mortgage_payment < loan_amount / payments_number:
+            return -np.inf, np.inf, 0
+
+        while remaining_loan > 0:
+            interest_payment = remaining_loan / 12 * nominal_interest
+            loan_interest_paid += interest_payment
+            remaining_loan -= mortgage_payment
+            months_taken += 1
+
+            if saving_amount != 0:
+                monthly_saving = saving_amount - mortgage_payment - interest_payment
+                if monthly_saving >= 0:
+                    total_invested += monthly_saving
+                    stock_valuation += monthly_saving
+                    stock_valuation *= monthly_interest_rate
+                else:
+                    return -np.inf, np.inf, 0
+
+            if early_stopping and months_taken / 12 == early_stopping:
+                break
+        
+        stock_interest_earned = stock_valuation - total_invested
+        results = stock_interest_earned - loan_interest_paid
+
+        return int(np.round(results)), months_taken, int(round(total_invested / months_taken))
---
+++ main.py
@@ -0,0 +1,71 @@
+from calculation_functions import saving_plan
+import numpy as np
+
+def main():
+    end_price = 3_000_000               # förväntad slutpris
+    cash = end_price * 0.15             # kontantinsats
+    monthly_cost = 4_000                # månadsavgift
+    operating_costs = 1000              # driftkostnad per månad
+    living_costs = 20_000               # levnadskostnader på alla låntagare
+
+    loan_amount = end_price - cash      # Lånebelopp
+    loan_grade = 1 - cash / end_price   # Belåningsgrad
+    payment_time = 50                   # Återbetalningstid
+    payments_number = payment_time * 12 # Antal avbetalningar
+
+    net_salary = 60000                  # Nettolön på alla låntagare
+    money_available = net_salary - monthly_cost - operating_costs - living_costs
+
+    assert money_available >= 0, f"Not enough money available, net amount: {money_available}"
+
+    stock_yearly_interest = 2 / 100     # Förväntad börstillväxt %
+    nominal_interest = 4 / 100          # bolåneränta %
+    effective_interest = np.power((1 + nominal_interest / 12), 12) - 1
+
+    # Extra
+    stop_after_years = False    # Set to False or an integer > 0 for number of years
+
+    best_results = -np.inf
+    saved_results = []
+    step = 1
+    search_range = range(0, money_available+step, step) if money_available != 0 else [0]
+    for mortgage_payment in search_range:
+        results, saving_plan_months, invested_per_month = saving_plan(
+            loan_amount,
+            payments_number,
+            nominal_interest=nominal_interest,
+            saving_amount=money_available,
+            mortgage_payment=mortgage_payment,
+            interest_rate=stock_yearly_interest,
+            early_stopping=stop_after_years
+        )
+
+        if results > best_results:
+            best_results = results
+            best_fixed_mortgage = mortgage_payment
+            best_saving_plan_months = saving_plan_months
+            best_invested_per_month = invested_per_month
+
+        saved_results.append(results)
+    
+    assert best_results != -np.inf, f"Cannot afford the interest and mortgage payments, with {money_available} per month"
+    
+    print('--'*20)
+    print(f"Purchase for end price: {end_price:,} SEK with an interest rate of {nominal_interest*100} %")
+    print(f"Minimum mortgage payment required {int(np.round(loan_amount / payments_number))} SEK")
+    print(f"Months available to pay off loan: {payments_number}")
+    print(f"Estimated stock market yearly interest: {stock_yearly_interest * 100} %")
+
+    print(f"\nBest end results: {best_results:,} SEK with {money_available:,} SEK available per month")
+    print(f"Saving for {best_saving_plan_months} months, or {best_saving_plan_months/12:.1f} years")
+    print(f"Mortgage payment (fixed): {best_fixed_mortgage:,} SEK per month")
+    print(f"Additional mortgage from minimum required: {int(np.round(best_fixed_mortgage - loan_amount / payments_number))} SEK")
+    if money_available > 0:
+        print(f"Average stock market investment per month: {best_invested_per_month:,} SEK")
+    else:
+        print(f"Stock market investment per month: {0} SEK")
+    # print(f"Results per fixed mortgage cost: {saved_results}")
+    print('--'*20)
+
+if __name__ == "__main__":
+    main()
