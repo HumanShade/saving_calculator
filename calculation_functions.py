f894b97d6526c9f23ff94b4a36737ecaf341ce42
Norman Janurberg <njanurberg@gmail.com>
2023-02-17 07:39:49 +0100

    Pay raise, mortgage invested, cost comparison

--- calculation_functions.py
+++ calculation_functions.py
@@ -37,42 +37,72 @@ def stock_growth(monthly_deposit, interest_rate, months_saving):
 # TODO: Add ränteavdrag
 # TODO: Break down the function into smaller functions for readability
 # TODO: Add increased house valuation (Separate function)
-# TODO: Add a starting amount of money that is already saved on the stock market
-def saving_plan(loan_amount, payments_number, nominal_interest=0, saving_amount=0, mortgage_payment=0, interest_rate=0, early_stopping=False):
-        # Housing
-        remaining_loan = loan_amount
-        loan_interest_paid = 0
-        months_taken = 0
+# TODO: Add starting month instead of assuming that it starts in January (affects ränteavdrag)
+# TODO: Add average yearly pay increase
+def saving_plan(loan_amount, payments_number, nominal_interest=0, saving_amount=0, start_saving=0, mortgage_payment=0, interest_rate=0, early_stopping=False, yearly_pay_raise=0):
+    # Housing
+    remaining_loan = loan_amount
+    loan_interest_paid = 0
+    yearly_interest_paid = 0
+    months_taken = 0
+    first_month = 0
+    last_month = 0
+    interest_payment = 0
 
-        # Stock market
-        total_invested = 0
-        stock_valuation = 0
-        monthly_interest_rate = np.power(1 + interest_rate, 1/12)
-        stock_interest_earned = 0
-        monthly_saving = 0
+    # Stock market
+    total_invested = 0 + start_saving
+    stock_valuation = 0 + start_saving
+    monthly_interest_rate = np.power(1 + interest_rate, 1/12)
+    stock_interest_earned = 0
+    monthly_saving = 0
 
-        if mortgage_payment < loan_amount / payments_number:
-            return -np.inf, np.inf, 0
+    if mortgage_payment < loan_amount / payments_number:
+        return -np.inf, np.inf, 0, 0, 0, 0, 0
 
-        while remaining_loan > 0:
-            interest_payment = remaining_loan / 12 * nominal_interest
-            loan_interest_paid += interest_payment
-            remaining_loan -= mortgage_payment
-            months_taken += 1
+    while remaining_loan > 0:
+        interest_payment = remaining_loan / 12 * nominal_interest
+        loan_interest_paid += interest_payment
+        yearly_interest_paid += interest_payment
+        remaining_loan -= mortgage_payment
+        months_taken += 1
 
-            if saving_amount != 0:
-                monthly_saving = saving_amount - mortgage_payment - interest_payment
-                if monthly_saving >= 0:
-                    total_invested += monthly_saving
-                    stock_valuation += monthly_saving
-                    stock_valuation *= monthly_interest_rate
-                else:
-                    return -np.inf, np.inf, 0
+        if first_month == 0:
+            first_month = mortgage_payment + interest_payment
 
-            if early_stopping and months_taken / 12 == early_stopping:
-                break
-        
-        stock_interest_earned = stock_valuation - total_invested
-        results = stock_interest_earned - loan_interest_paid
+        if saving_amount != 0:
+            monthly_saving = saving_amount - mortgage_payment - interest_payment
+            if monthly_saving >= 0:
+                total_invested += monthly_saving
+                stock_valuation += monthly_saving
+                stock_valuation *= monthly_interest_rate
+            else:
+                return -np.inf, np.inf, 0, 0, 0, 0, 0
 
-        return int(np.round(results)), months_taken, int(round(total_invested / months_taken))
+        # Yearly changes 
+        if months_taken % 12 == 0:
+
+            # Ränteavdrag
+            if yearly_interest_paid <= 100_000:
+                interest_deduction = yearly_interest_paid * 0.3
+            else:
+                # yearly_interest_paid > 100_000
+                interest_deduction = 30_000 + (yearly_interest_paid - 100_000) * 0.21
+            
+            # For simplicity, it goes to the stocks?
+            stock_valuation += interest_deduction
+            total_invested += interest_deduction
+
+            # Pay raises
+            saving_amount += yearly_pay_raise
+
+
+        if early_stopping and months_taken / 12 == early_stopping:
+            break
+    
+    if last_month == 0:
+        last_month = mortgage_payment + interest_payment
+
+    stock_interest_earned = stock_valuation - total_invested
+    results = stock_interest_earned - loan_interest_paid
+
+    return int(np.round(results)), months_taken, int(round(total_invested / months_taken)), stock_valuation, remaining_loan, first_month, last_month
--- main.py
+++ main.py
@@ -16,28 +16,33 @@ def main():
     net_salary = 60000                  # Nettolön på alla låntagare
     money_available = net_salary - monthly_cost - operating_costs - living_costs
 
+    stock_savings = 0                   # Belopp redan investerad (alla låntagare)
+    yearly_pay_raise = 0                # Löneökning belopp per år
+
     assert money_available >= 0, f"Not enough money available, net amount: {money_available}"
 
     stock_yearly_interest = 2 / 100     # Förväntad börstillväxt %
-    nominal_interest = 4 / 100          # bolåneränta %
+    nominal_interest = 2 / 100          # bolåneränta %
     effective_interest = np.power((1 + nominal_interest / 12), 12) - 1
 
     # Extra
-    stop_after_years = False    # Set to False or an integer > 0 for number of years
+    stop_after_years = 5    # Set to False or an integer > 0 for number of years
 
     best_results = -np.inf
     saved_results = []
     step = 1
     search_range = range(0, money_available+step, step) if money_available != 0 else [0]
     for mortgage_payment in search_range:
-        results, saving_plan_months, invested_per_month = saving_plan(
+        results, saving_plan_months, invested_per_month, stock_invested, remaining_loan, first_month, last_month = saving_plan(
             loan_amount,
             payments_number,
             nominal_interest=nominal_interest,
             saving_amount=money_available,
+            start_saving=stock_savings,
             mortgage_payment=mortgage_payment,
             interest_rate=stock_yearly_interest,
-            early_stopping=stop_after_years
+            early_stopping=stop_after_years,
+            yearly_pay_raise=yearly_pay_raise
         )
 
         if results > best_results:
@@ -45,6 +50,10 @@ def main():
             best_fixed_mortgage = mortgage_payment
             best_saving_plan_months = saving_plan_months
             best_invested_per_month = invested_per_month
+            best_stock_invested = stock_invested
+            best_remaining_loan = remaining_loan
+            best_first_month = first_month
+            best_last_month = last_month
 
         saved_results.append(results)
     
@@ -68,7 +77,12 @@ def main():
         print(f"Average stock market investment per month: {best_invested_per_month:,} SEK")
     else:
         print(f"Stock market investment per month: {0} SEK")
-    # print(f"Results per fixed mortgage cost: {saved_results}")
+
+    print(f"Stock market investment valuation: {int(np.round(best_stock_invested)):,} SEK")
+    print(f"Remaining loan: {int(np.round(best_remaining_loan)):,} SEK")
+    print(f"Invested in mortgage: {int(np.round(end_price-best_remaining_loan)):,} SEK")
+    print(f"Starting mortgage + interest cost: {int(np.round(best_first_month)):,} SEK")
+    print(f"Last mortgage + interest cost: {int(np.round(best_last_month)):,} SEK")
     print('--'*20)
 
 if __name__ == "__main__":
